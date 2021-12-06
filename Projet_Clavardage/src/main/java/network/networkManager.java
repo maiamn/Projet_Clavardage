fa553b38488b642b3efd83131cf41791a58c9aaf
mmangin <mmangin@localhost>
2021-12-06 18:02:04 +0100

    ajout commentaires network_manager (format messages broadcast)

--- Projet_Clavardage/src/main/java/network/networkManager.java
+++ Projet_Clavardage/src/main/java/network/networkManager.java
@@ -1,5 +1,17 @@
 package network;
 
 public class networkManager {
+	
+	// Format des messages en broadcast 
+	/* flag de connexion/deconnexion
+	 * adresse ip de celui qui se connecte ou se déconnecte 
+	 * pseudo  
+	 * 
+	 * <!> REPONSE EN TCP
+	 * 
+	 * Pour le broadcast du speudo lors de la connexion, seul l'utilisateur qui 
+	 * a déjà le pseudo que l'on veut peut répondre NON -> on met en place en timer
+	 * donc si on n'a pas de réponse on dit que c'est OK
+	 */
 
 }
