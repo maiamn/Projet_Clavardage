37d2a1e2afca926276577145fbe0a6ed9cf49fb8
maiamn <maia.mangin25@gmail.com>
2021-12-10 12:55:45 +0100

    comments

--- Projet_Clavardage/src/main/java/network/networkManager.java
+++ Projet_Clavardage/src/main/java/network/networkManager.java
@@ -4,14 +4,14 @@ public class networkManager {
 	
 	// Format des messages en broadcast 
 	/* flag de connexion/deconnexion
-	 * adresse ip de celui qui se connecte ou se déconnecte 
+	 * adresse ip de celui qui se connecte ou se deconnecte 
 	 * pseudo  
 	 * 
 	 * <!> REPONSE EN TCP
 	 * 
-	 * Pour le broadcast du speudo lors de la connexion, seul l'utilisateur qui 
-	 * a déjà le pseudo que l'on veut peut répondre NON -> on met en place en timer
-	 * donc si on n'a pas de réponse on dit que c'est OK
+	 * Pour le broadcast du pseudo lors de la connexion, seul l'utilisateur qui 
+	 * a deja le pseudo que l'on veut peut repondre NON -> on met en place en timer
+	 * donc si on n'a pas de reponse on dit que c'est OK
 	 */
 
 }
