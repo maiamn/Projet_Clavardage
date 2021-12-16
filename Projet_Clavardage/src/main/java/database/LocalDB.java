d287d5a17844d6001270901582ec4e1574697fda
maiamn <maia.mangin25@gmail.com>
2021-12-16 11:35:59 +0100

    Creation local database + queries

--- Projet_Clavardage/src/main/java/database/LocalDB.java
+++ Projet_Clavardage/src/main/java/database/LocalDB.java
@@ -1,35 +1,156 @@
 package database;
 
 import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet ; 
+import java.sql.DriverManager;
+
 
 public class LocalDB {
 	
 	// Attributes 
-	private String username ; 
-	private InetAddress IPaddress ; 
+	Connection connection ; 
+	Statement statement ;
 	
 	// Constructor 
-	public LocalDB(String username, InetAddress IP) {
-		this.username = username ; 
-		this.IPaddress = IP ; 
-	}
-	
-	// Getters
-	public String getUsername() {
-		return this.username ; 
-	}
-	
-	public InetAddress getIP() {
-		return this.IPaddress ; 
-	}
-	
-	// Setters 
-	public void setUsername(String username) {
-		this.username = username ; 
-	}
-	
-	public void setIP(InetAddress IP) {
-		this.IPaddress = IP ; 
+	public LocalDB() {
+		
+		// Load the driver class file 
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdcDriver") ; 
+		} catch (ClassNotFoundException e) {
+			System.out.println(e) ; 
+		}
+		
+		try {
+			
+			// Make a database connection
+			this.connection = DriverManager.getConnection("jdbc:odbc:LocalDB");
+			
+			// Create a statement object
+			this.statement = this.connection.createStatement() ; 
+
+		} catch (SQLException e) {
+			System.out.println(e) ; 
+		}
+		
+		
 	}
 
+	
+	// Create database
+	public void createDatabase() {
+		String query = "CREATE DATABASE UsernameToIP(Username STRING, IP STRING) ;" ;
+		
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			rs.close(); 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+	// Add a user to the database
+	public void addUser(String username, InetAddress IP) {
+		String query = "INSERT INTO UsernameToIP (Username, IP) VALUES ('" + username + "', '" + IP + "') ;" ; 
+		
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			rs.close(); 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	// Delete a user to the database
+	public void deleteUserByName(String username) {
+		String query = "DELETE FROM UsernameToIP WHERE Username='" + username + "' ;" ;	
+		
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			rs.close(); 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void deleteUserByIP(InetAddress IP) {
+		String query = "DELETE FROM UsernameToIP WHERE IP='" + IP + "' ;" ;		
+		
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			rs.close(); 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	// Get information in the database
+	public String getUsername(InetAddress IP) {
+		String query = "SELECT UsernameToIP.Username FROM UsernameToIP WHERE IP = '" + IP + "' ;";	
+		String username = "" ; 
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			username = rs.getString(0) ;
+			rs.close(); 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		
+		return username ;	
+	}
+	
+	public InetAddress getIP(String username) {
+		String query = "SELECT UsernameToIP.IP FROM UsernameToIP WHERE Username = '" + username + "' ;";	
+		String IPString = "" ; 
+		InetAddress IP = null ; 
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			IPString = rs.getString(0) ;
+			IP = InetAddress.getByName(IPString) ; 
+			rs.close(); 
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return IP ; 
+	}
+	
+	// Drop database 
+	public void dropDatabase() {
+		String query = "DROP TABLE UsernameToIP ;" ;
+		
+		try {
+			// Execute the statement 
+			ResultSet rs = this.statement.executeQuery(query) ; 
+			rs.close(); 
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+	// Close the connection of database 
+	public void closeConnection() {
+		try {
+			this.connection.close() ;
+		} catch (SQLException e) {
+			System.out.println(e) ; 
+		}
+		
+	}	
+	
+
+	public static void main(String[] args) {
+		
+	}
+	
 }
