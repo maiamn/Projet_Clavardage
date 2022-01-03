6335732a026b34d1fd599700d9283c56d90e77fb
maiamn <maia.mangin25@gmail.com>
2022-01-03 23:22:06 +0100

    Creation and formatting of LocalDBTest file

---
+++ Projet_Clavardage/src/test/java/database/LocalDBTest.java
@@ -0,0 +1,176 @@
+package database;
+
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import java.io.IOException;
+import org.junit.Test;
+import database.LocalDB.* ;
+import network.NetworkManager;
+import network.NetworkManager.MessageType;
+
+public class LocalDBTest {
+	
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////// Definition of global variables needed to realize tests ///////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/* **************************************************************************************** */
+	////////////////////////////////// addUSer - global variables ////////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables
+	
+	// Result variables
+
+	/* **************************************************************************************** */
+
+	
+	/* **************************************************************************************** */
+	///////////////////////////// deleteUserByName - global variables ////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables 
+	
+	// Result variables 
+	
+	/* **************************************************************************************** */
+
+	
+	/* **************************************************************************************** */
+	////////////////////////////// deleteUserByIP - global variables /////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables 
+	
+	// Result variables 
+	
+	/* **************************************************************************************** */
+	
+	
+	/* **************************************************************************************** */
+	//////////////////////////////// getUsername - global variables //////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables 
+	
+	// Result variables 
+	
+	/* **************************************************************************************** */
+	
+	
+	/* **************************************************************************************** */
+	/////////////////////////////////// getIP - global variables /////////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables 
+	
+	// Result variables 
+	
+	/* **************************************************************************************** */
+	
+	
+	/* **************************************************************************************** */
+	/////////////////////////////// dropDatabase - global variables //////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables 
+	
+	// Result variables 
+	
+	/* **************************************************************************************** */
+	
+	
+	/* **************************************************************************************** */
+	////////////////////////////// closeConnection - global variables ////////////////////////////
+	// Variables needed for the function 
+	
+	// Expected variables 
+	
+	// Result variables 
+	
+	/* **************************************************************************************** */
+	
+	
+	
+	
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////// Before Class = realized before the class //////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@BeforeClass 
+	public static void initAll() throws IOException {
+	
+	/* **************************************************************************************** */
+	//////////////////////// Init of variables useful for tests of assUser ///////////////////////
+
+		
+	/* **************************************************************************************** */
+	
+
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////// TESTS ////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	//////////////////////////////////// FUNCTION constructor ////////////////////////////////////
+	@Test
+	public void testLocalDB() {
+		fail("Not yet implemented");
+	}
+
+	
+	////////////////////////////////////// FUNCTION addUser /////////////////////////////////////	
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	
+	//////////////////////////////// FUNCTION deleteUserByName //////////////////////////////////	
+	@Test
+	public void testDeleteUserByName() {
+		fail("Not yet implemented");
+	}
+
+	
+	///////////////////////////////// FUNCTION deleteUserByIP //////////////////////////////////	
+	@Test
+	public void testDeleteUserByIP() {
+		fail("Not yet implemented");
+	}
+
+	
+	/////////////////////////////////// FUNCTION getUsername ///////////////////////////////////	
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	
+	////////////////////////////////////// FUNCTION getIP //////////////////////////////////////	
+	@Test
+	public void testGetIP() {
+		fail("Not yet implemented");
+	}
+
+	
+	/////////////////////////////////// FUNCTION dropDatabase //////////////////////////////////	
+	@Test
+	public void testDropDatabase() {
+		fail("Not yet implemented");
+	}
+
+	
+	///////////////////////////////// FUNCTION closeConnection /////////////////////////////////	
+	@Test
+	public void testCloseConnection() {
+		fail("Not yet implemented");
+	}
+
+
+}
