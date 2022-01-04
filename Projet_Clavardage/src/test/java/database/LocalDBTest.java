97384f0dc4c8c837c73a671fabf6311a38bc48e8
maiamn <maia.mangin25@gmail.com>
2022-01-04 11:51:55 +0100

    Test addUser + deleteByName + deleteByIP OK

--- Projet_Clavardage/src/test/java/database/LocalDBTest.java
+++ Projet_Clavardage/src/test/java/database/LocalDBTest.java
@@ -2,6 +2,8 @@ package database;
 
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.After;
 import java.io.IOException;
 import java.sql.DriverManager;
 import java.net.InetAddress;
@@ -11,7 +13,9 @@ import java.sql.Statement;
 import java.sql.ResultSet ; 
 import org.junit.Test;
 import database.LocalDB.* ;
-import network.NetworkManager.MessageType;
+import java.net.InetAddress;
+import java.util.Random ; 
+
 
 public class LocalDBTest {
 	
@@ -26,13 +30,19 @@ public class LocalDBTest {
 	// Database
 	protected static LocalDB db = new LocalDB() ; 
 	protected static Connection connection ; 
+	protected static Statement statement ;
 	protected static String addrDb ;
 	protected static String login ;
 	protected static String password ;
 	
 	// Person 
-	protected static String [] usernames = new String [10] ; 
-	protected static String [] IPAdresses = new String [10] ; 
+	protected static int nbTests = 100 ; 
+	protected static String [] usernames = new String [nbTests] ; 
+	protected static InetAddress [] IPAddresses = new InetAddress [nbTests] ; 
+	
+	// Random number 
+	Random rand = new Random() ; 
+	protected static int index ; 
 	/* **************************************************************************************** */
 
 	
@@ -44,72 +54,6 @@ public class LocalDBTest {
 	
 	// Result variables
 
-	/* **************************************************************************************** */
-
-	
-	/* **************************************************************************************** */
-	///////////////////////////// deleteUserByName - global variables ////////////////////////////
-	// Variables needed for the function 
-	
-	// Expected variables 
-	
-	// Result variables 
-	
-	/* **************************************************************************************** */
-
-	
-	/* **************************************************************************************** */
-	////////////////////////////// deleteUserByIP - global variables /////////////////////////////
-	// Variables needed for the function 
-	
-	// Expected variables 
-	
-	// Result variables 
-	
-	/* **************************************************************************************** */
-	
-	
-	/* **************************************************************************************** */
-	//////////////////////////////// getUsername - global variables //////////////////////////////
-	// Variables needed for the function 
-	
-	// Expected variables 
-	
-	// Result variables 
-	
-	/* **************************************************************************************** */
-	
-	
-	/* **************************************************************************************** */
-	/////////////////////////////////// getIP - global variables /////////////////////////////////
-	// Variables needed for the function 
-	
-	// Expected variables 
-	
-	// Result variables 
-	
-	/* **************************************************************************************** */
-	
-	
-	/* **************************************************************************************** */
-	/////////////////////////////// dropDatabase - global variables //////////////////////////////
-	// Variables needed for the function 
-	
-	// Expected variables 
-	
-	// Result variables 
-	
-	/* **************************************************************************************** */
-	
-	
-	/* **************************************************************************************** */
-	////////////////////////////// closeConnection - global variables ////////////////////////////
-	// Variables needed for the function 
-	
-	// Expected variables 
-	
-	// Result variables 
-	
 	/* **************************************************************************************** */
 	
 	
@@ -134,24 +78,34 @@ public class LocalDBTest {
             fail(e.toString());
         }
 		
-		
 		// Users and IPAdresses 
-		for (int i=0; i<usernames.length ; i++) {
+		for (int i=0; i<nbTests ; i++) { 
 			usernames[i]="username"+i ;
-			IPAdresses[i]="192.168.1."+i ; 
-			
+			IPAddresses[i]= InetAddress.getLocalHost() ; 
 		}
-		
-		
-
 	
-	/* **************************************************************************************** */
-	//////////////////////// Init of variables useful for tests of assUser ///////////////////////
-
-		
-	/* **************************************************************************************** */
+	}
 	
-
+	
+	
+	@Before 
+	public void initBeforeTests() throws IOException {
+	/* **************************************************************************************** */
+	//////////////////////////////////// Add users to database ///////////////////////////////////
+		for (int i=0; i<nbTests; i++) {
+			db.addUser(usernames[i], IPAddresses[i]);
+		}	
+	/* **************************************************************************************** */
+	}
+	
+	@After
+	public void resetAfterTests() throws IOException {
+		////////////////////////////////////// Drop database /////////////////////////////////////
+		try {
+			db.statement.executeUpdate("TRUNCATE TABLE UsernameToIP") ; 
+		} catch (SQLException e) {
+				System.out.println(e);
+		}
 	}
 
 
@@ -159,61 +113,100 @@ public class LocalDBTest {
 	/////////////////////////////////////////// TESTS ////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	
-	
-	//////////////////////////////////// FUNCTION constructor ////////////////////////////////////
-	@Test
-	public void testLocalDB() {
-		fail("Not yet implemented");
-	}
 
 	
 	////////////////////////////////////// FUNCTION addUser /////////////////////////////////////	
 	@Test
 	public void testAddUser() {
-		fail("Not yet implemented");
-	}
+		try {			
+			for (int i=0; i<nbTests; i++) {	
+				ResultSet rs=db.statement.executeQuery("SELECT * FROM UsernameToIP WHERE Username='" + usernames[i] + "'") ; 
+				assertTrue(rs.next());
+				// Check usernames
+				assertEquals(usernames[i], rs.getString("username")) ; 
+				// Check IPs
+				assertEquals(IPAddresses[i].toString(), rs.getString("ip")) ; 
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	} 
 
 	
 	//////////////////////////////// FUNCTION deleteUserByName //////////////////////////////////	
 	@Test
 	public void testDeleteUserByName() {
-		fail("Not yet implemented");
-	}
+		try {
+			// Delete 10 random users by name
+			for (int i=0; i<10; i++) {	
+				// Delete the user of random index
+				index = rand.nextInt(nbTests);
+				db.deleteUserByName(usernames[index]) ; 
+				
+				// Check if one and only one user has been deleted 
+				ResultSet rs=db.statement.executeQuery("SELECT COUNT(*) FROM UsernameToIP") ; 
+				if (rs.next()) {
+					assertEquals((nbTests-i-1), rs.getInt(1)) ;
+				}
+				// Check that the good user has been deleted from the database 
+				ResultSet rs2=db.statement.executeQuery("SELECT * FROM UsernameToIP WHERE Username='" + usernames[index] + "'") ;
+				assertFalse(rs2.next());
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	} 
 
 	
 	///////////////////////////////// FUNCTION deleteUserByIP //////////////////////////////////	
 	@Test
 	public void testDeleteUserByIP() {
-		fail("Not yet implemented");
-	}
+		try {
+			// All users have same ip -> by deleting by ip the table will be empty
+			db.deleteUserByIP(IPAddresses[index]) ; 
+			ResultSet rs=db.statement.executeQuery("SELECT COUNT(*) FROM UsernameToIP") ; 
+			if (rs.next()) {
+				assertEquals(0, rs.getInt(1)) ;
+			}
+			
+			// Check that the good user has been deleted from the database 
+			ResultSet rs2=db.statement.executeQuery("SELECT * FROM UsernameToIP WHERE IP='" + IPAddresses[index] + "'") ;
+			assertFalse(rs2.next());
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	} 
 
 	
 	/////////////////////////////////// FUNCTION getUsername ///////////////////////////////////	
-	@Test
+	/* @Test
 	public void testGetUsername() {
 		fail("Not yet implemented");
-	}
+	} */
 
 	
 	////////////////////////////////////// FUNCTION getIP //////////////////////////////////////	
-	@Test
+	/* @Test
 	public void testGetIP() {
 		fail("Not yet implemented");
-	}
+	} */
 
 	
 	/////////////////////////////////// FUNCTION dropDatabase //////////////////////////////////	
-	@Test
+	/* @Test
 	public void testDropDatabase() {
 		fail("Not yet implemented");
-	}
+	} */
 
 	
 	///////////////////////////////// FUNCTION closeConnection /////////////////////////////////	
-	@Test
+	/* @Test
 	public void testCloseConnection() {
 		fail("Not yet implemented");
-	}
+	} */ 
 
 
 }
