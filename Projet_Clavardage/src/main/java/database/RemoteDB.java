ff8104f4d7fb481891d1b38bf5ead3363334391c
mmangin <mmangin@localhost>
2022-01-14 12:35:26 +0100

    Debug

--- Projet_Clavardage/src/main/java/database/RemoteDB.java
+++ Projet_Clavardage/src/main/java/database/RemoteDB.java
@@ -135,8 +135,8 @@ public class RemoteDB {
 		
 		// Query to order DB by descending date and select messages 
 		String query = "SELECT * FROM History "
-				+ "WHERE (Sender = '" + person2 + "' AND Receiver = '" + person1 + "') "
-				+ "ORDER BY Date DESC LIMIT 50" ; 
+				+ "WHERE Sender = '" + person2 + "' AND Receiver = '" + person1 + "' "
+				+ "ORDER BY Date DESC" ; 
 
 		try {
 			// Execute the statement 
